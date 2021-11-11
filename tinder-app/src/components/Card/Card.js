a0564d803284b37a45385bd98693a9ed744a0ca0
AkshayBenny <akshaybennygh@gmail.com>
2021-11-11 12:59:40 +0530

    ain

--- tinder-app/src/components/Card/Card.js
+++ tinder-app/src/components/Card/Card.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import TinderCard from "react-tinder-card";
 import "./Card.css";
+import { People } from "./CardData.js";
 
 function Card() {
-  return <div></div>;
+  console.log(People);
+  const [people, setPeople] = useState(People);
+  console.log(people);
+  return (
+    <div className="cards-cont">
+      <div className="card-cont">
+        {people.map((person, index) => (
+          <TinderCard
+            className="swipe"
+            key={index}
+            preventSwipe={["up", "down"]}
+            onSwipe={(dir) => swiped(dir, person.name)}
+            onCardLeftScreen={() => outOfFrame(person.name)}
+          ></TinderCard>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
