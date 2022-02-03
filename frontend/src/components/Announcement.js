4dc6ea65924e8da33914f7ab7754a146cb6c4468
AkshayBenny <akshaybennygh@gmail.com>
2022-02-03 17:06:27 +0530

    update

--- frontend/src/components/Announcement.js
+++ frontend/src/components/Announcement.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import { sliderItems } from '../data';
+import {
+CarouselProvider,
+Slider,
+Slide,
+ButtonBack,
+ButtonNext,
+} from 'pure-react-carousel';
+import 'pure-react-carousel/dist/react-carousel.es.css';
 
 function Announcement() {
-  return <div></div>;
-}
+  return (
+    <div className='w-screen'>
+      <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={125}
+          totalSlides={sliderItems.length}
+          lockOnWindowScroll={true}
+          touchEnabled={true}
+          isIntrinsicHeight={true}
+        >
+          <Slider>
+            {sliderItems.map((item, index) => {
+              return (
+                <Slide index={index}>
+                 <div>
+                 <img
+                    className='h-[343px] w-full object-cover'
+                    src={item.img}
+                    alt='playground'
+                  />
+                  <div>
+                    <p>{item.desc}</p>
+                  </div>
+                 </div>
+                </Slide>
+              );
+            })}
+          </Slider>
+         
+        </CarouselProvider>
+     
+     
+     
+     
+    </div>
+  );
 
-export default Announcement;
+
+export default Announcement
---
+++ frontend/src/data.js
@@ -0,0 +1,24 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img:"https://freedesignfile.com/upload/2016/10/Beautiful-woman-holding-shopping-bags-on-white-background.jpg",
+        desc:"don't compromise on style! get flat 30% off for new arrivals",
+        bg:""
+    },
+    {
+        id: 1,
+        img:"https://image.freepik.com/free-photo/portrait-young-happy-smiling-woman-with-shopping-bags-white-background_231208-1853.jpg",
+        desc:"don't compromise on style! get flat 30% off for new arrivals",
+        bg:""
+    },    {
+        id: 1,
+        img:"https://previews.123rf.com/images/nnudoo/nnudoo1710/nnudoo171000007/87331166-carrinho-de-compras-e-computador-port%C3%A1til-com-produtos-isolados-no-fundo-branco-compre-um-conceito-o.jpg",
+        desc:"don't compromise on style! get flat 30% off for new arrivals",
+        bg:""
+    },    {
+        id: 1,
+        img:"https://dm0qx8t0i9gc9.cloudfront.net/thumbnails/video/49EwKH5deijotr386/videoblocks-female-shopper-holding-multicolored-shopping-bags-on-white-background-in-studio-lets-go-shopping-concept_bvgdkxrsf_thumbnail-1080_01.png",
+        desc:"don't compromise on style! get flat 30% off for new arrivals",
+        bg:""
+    }
+]
