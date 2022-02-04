66e43da3f17033495016db4ca7e5997dc6f15f98
AkshayBenny <akshaybennygh@gmail.com>
2022-02-04 18:53:47 +0530

    update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
 
 //REGISTER
 router.post('/register', async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: req.body.password,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
   });
 
   try {
@@ -14,6 +18,27 @@ router.post('/register', async (req, res) => {
     res.status(201).json(savedUser);
   } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json('Wrong cretentials!');
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const password = hashedPassword.toString(CryptoJS.enc.Utf8);
+    if (password != req.body.password) {
+      res.status(401).json('Wrong cretentials!');
+      // console.log('wrong password');
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
