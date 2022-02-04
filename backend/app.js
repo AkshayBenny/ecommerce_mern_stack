9682ecbfc59e5b6d3cc323ff162738ea747f4c4d
AkshayBenny <akshaybennygh@gmail.com>
2022-02-04 17:59:36 +0530

    auth route added

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ mongoose
 
 app.use(express.json()); //accepts post req as json
 app.use('/api/user', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Backend running server on port ${process.env.PORT}`);
