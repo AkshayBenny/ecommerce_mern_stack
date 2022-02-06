efd027a7bb51ae780e3a5fba2e56f043b93b988a
AkshayBenny <akshaybennygh@gmail.com>
2022-02-06 23:01:53 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
 const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+const stripeRoute = require('./routes/stripe');
 
 dotenv.config();
 
@@ -14,10 +18,14 @@ mongoose
   .then(() => console.log('Connected to database'))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json()); //accepts post req as json
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/orders', orderRoute);
+app.use('/api/checkout', stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Backend running server on port ${process.env.PORT}`);
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -7,73 +7,89 @@ const {
 } = require('./verifyToken');
 
 //Create
-router.post('/', verifyTokenAndAdmin, async (req, res) => {
-  const newProduct = new Product(req.body);
+router.post('/', async (req, res) => {
+  const newOrder = new Order(req.body);
   try {
-    const savedProduct = await newProduct.save();
-    res.status(200).json(savedProduct);
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
 //update product
-router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
   try {
-    const updatedProduct = await Product.findByIdAndUpdate(
+    const updatedOrder = await Order.findByIdAndUpdate(
       req.params.id,
       {
         $set: req.body,
       },
       { new: true }
     );
-    res.status(200).json(updatedProduct);
+    res.status(200).json(updatedOrder);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
 //delete
-router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
   try {
-    await Product.findByIdAndDelete(req.params.id);
-    res.status(200).json('Product has been deleted...');
+    await Order.findByIdAndDelete(req.params.id);
+    res.status(200).json('Order has been canceled...');
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-//get product
-router.get('/find/:id', async (req, res) => {
+//get user orders
+router.get('/find/:userId', async (req, res) => {
   try {
-    const product = await Product.findById(req.params.id);
+    const orders = await Order.find({ userId: req.params.userId });
 
-    res.status(200).json(product);
+    res.status(200).json(orders);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-//get all products
-router.get('/', async (req, res) => {
-  const qNew = req.query.new;
-  const qCategory = req.query.category;
+//get all orders
+router.get('/', verifyTokenAndAdmin, async (req, res) => {
   try {
-    let products;
+    const orders = await Order.find();
+    res.status(200).json(orders);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-    if (qNew) {
-      products = await Product.find().sort({ createdAt: -1 }).limit(5);
-    } else if (qCategory) {
-      products = await Product.find({
-        categories: {
-          $in: [qCategory],
+//get monthly income
+router.get('/income', verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+  try {
+    const income = await Order.aggregate([
+      {
+        $match: {
+          createdAt: { $gte: previousMonth },
         },
-      });
-    } else {
-      products = await Product.find();
-    }
-
-    res.status(200).json(products);
+      },
+      {
+        $project: {
+          month: { $month: '$createdAt' },
+          sales: '$amount',
+        },
+      },
+      {
+        $group: {
+          _id: '$month',
+          total: { $sum: '$sales' },
+        },
+      },
+    ]);
+    res.status(200).json(income);
   } catch (error) {
     res.status(500).json(error);
   }
