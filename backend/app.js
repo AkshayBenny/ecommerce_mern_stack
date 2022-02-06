0bcf0d82f381391998c42cf0df590f99bc31b6aa
AkshayBenny <akshaybennygh@gmail.com>
2022-02-06 18:29:54 +0530

    update

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ mongoose
 app.use(express.json()); //accepts post req as json
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Backend running server on port ${process.env.PORT}`);
